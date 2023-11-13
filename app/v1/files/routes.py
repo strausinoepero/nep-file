b5d1fef2f84f97897d5dbcf709d472427556b185
a.gogaev <a.gogaev@bi.zone>
2023-11-13 13:29:07 +0300

    check size and extension file

--- app/v1/files/routes.py
+++ app/v1/files/routes.py
@@ -3,6 +3,9 @@ from flask import request
 from app import app
 from app.v1.files import bp
 
+MAX_FILE_SIZE = 8 * 1024 * 1024 + 1
+
+
 @bp.post("/")
 def hello():
     files = request.files['file']
@@ -10,6 +13,15 @@ def hello():
     folderPath = app.config.get('UPLOAD_FOLDER') + path
     existFolder = os.path.isdir(folderPath)
     fullPath = f'{folderPath}/{files.filename}'
+    filenameNotExtension, fileExtension = os.path.splitext(files.filename)
+    listExtension = app.config.get('ALLOWED_UPLOAD_FILES_EXTENSIONS')
+    mimetype = files.mimetype
+    fileBytes = files.read(MAX_FILE_SIZE)
+    print('ddfmd', len(fileBytes))
+    if len(fileBytes) == MAX_FILE_SIZE:
+        return f'file size exceeds {MAX_FILE_SIZE} byte', 409
+    if not mimetype in listExtension:
+        return f'files with the extension {mimetype} are not supported', 409
     if not existFolder:
         os.makedirs(folderPath, exist_ok=True)
     if os.path.isfile(fullPath):
